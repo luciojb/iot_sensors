430159962ac8acb1c19257a3e746cff4353a8257
Lúcio José Beirão <luciojbeirao@gmail.com>
2018-06-08 09:24:30 -0300

    Added MPDF

--- sensor_iot/config/app.php
+++ sensor_iot/config/app.php
@@ -160,6 +160,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * The MPDF provider
+         */
+        niklasravnsborg\LaravelPdf\PdfServiceProvider::class,
+
     ],
 
     /*
@@ -208,6 +213,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /*
+         *The MPDF aliase
+         */
+         'PDF' => niklasravnsborg\LaravelPdf\Facades\Pdf::class,
 
     ],
 
